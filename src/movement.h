f7cb507f42ab34605ab5a6ee00d24371604ae8e0
Danil Korolenko <d_korolenko15@icloud.com>
2020-06-23 14:38:04 +0800

    Void pawn files movement.h in \src

---
+++ src/movement.h
@@ -0,0 +1,10 @@
+#ifndef MOVEMENT_H
+
+#define MOVEMENT_H
+
+char** EnterCoordinate(char**, char);
+int Check(char**, char, char, int, int, int, int);
+int Translate(char);
+int StepPawn(char**, char, char, int, int, int, int);
+
+#endif
